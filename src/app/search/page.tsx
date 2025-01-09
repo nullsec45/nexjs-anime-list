7fb52ea578b1b754c162226886056af84729ac4b
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-09 11:41:34 +0700

    modified : new page search

---
+++ src/app/search/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+        <h1>Search Page</h1>
+        <p>Ini adalah search page</p>
+    </div>
+  )
+}
+
+export default Page
