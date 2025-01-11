40b195e4755a63ce2f8111d5a527efe8c47d3f5f
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-11 11:28:51 +0700

    modified : enharramce anime list.

--- src/app/components/AnimeList/index.tsx
+++ src/app/components/AnimeList/index.tsx
@@ -1,17 +1,49 @@
 import Image from "next/image"
+import Link from "next/link";
 
 
 interface AnimeListProps {
-  title: string;
-  image: string;
+  animes: {
+    pagination: {
+      last_visible_page: number;
+      has_next_page: boolean;
+      current_page: number;
+      items?: {
+        count: number;
+        total: number;
+        per_page: number;
+      };
+    };
+    data: AnimeData[];
+  };
 }
 
-const AnimeList:React.FC<AnimeListProps> = ({title,image}) => {
+interface AnimeData {
+  mal_id: number;
+  title: string;
+  images: {
+    webp: {
+      image_url: string;
+    };
+  };
+  [key: string]: any; // Opsional jika ada properti tambahan
+}
+
+const AnimeList:React.FC<AnimeListProps> = ({animes}) => {
   return (
-    <div className="">
-        <Image src={image} alt="..." width={350} height={350}/>
-        <h3 className="font-bold md:text-xl text-md p-4">{title}</h3>
-      </div>
+        <div className="container mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 p-4">
+          {
+            animes.data.map((anime:AnimeData) => {
+              return (
+               <Link href={`/${anime.mal_id}`} className="cursor-pointer">
+                   <Image src={anime.images.webp.image_url} alt="..." width={350} height={350} className="w-full"/>
+                   <h3 className="font-bold md:text-xl text-md p-4">{anime.title}</h3>
+                 </Link>
+              )
+            })
+          }
+        </div>
+
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,17 @@
 import AnimeList from "@/app/components/AnimeList";
-
-interface AnimeData {
-  mal_id:number;
-  title: string;
-  images: {
-    webp: {
-      image_url: string;
-    };
-  };
-}
-
+import HeaderAnimeList from "@/app/components/AnimeList/header";
 
 const Home=async () => {
-  const response=await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`);
-  const anime=await response.json();
+  const response=await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`);
+  const topAnime=await response.json();
 
   return (
-      <div>
-          <h1>Paling Populer</h1>
-           <div className="grid grid-cols-3 gap-6 h-32 md:grid-cols=5 sm:grid-cols-3 grid-cols-2 gap-4">
-              {
-                anime.data.map((data:AnimeData) => {
-                  return (
-                    <div key={data.mal_id} className="shadow-xl">
-                       <AnimeList  title={data.title} image={data.images.webp.image_url} />
-                    </div>
-                  )
-                })
-              }
-            </div>
-          
-      </div>
+      <>
+        <section>
+          <HeaderAnimeList title="Paling Populer" linkHref="/populer" linkTitle="Lihat Semua"/>
+          <AnimeList animes={topAnime} />
+        </section>
+      </>
   );
 }
 
