31e5f25d44eb548fd638a85fd76d901b586e9415
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-15 17:30:17 +0700

    fix : fix dinamis routing with params.

--- src/app/search/[keyword]/page.tsx
+++ src/app/search/[keyword]/page.tsx
@@ -1,24 +1,41 @@
-import React from 'react'
+import React,{ReactNode} from 'react'
 import AnimeList from "@/app/components/AnimeList";
 import HeaderAnimeList from "@/app/components/AnimeList/header";
 
-interface Params{
-  keyword:string
-}
+const Page = async function ({
+  params,
+}: {
+  params: Promise<{ keyword: string }>
+})
+{
+  const keyword = (await params).keyword
 
-const Page : React.FC<{params:Params}> = async ({params}) => {
-  const {keyword} =  params
-  const response=await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${keyword}`);
-  const searchAnime=await response.json();
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${keyword}`, {
+      cache: 'no-store', // Hindari cache jika data sering berubah
+    });
 
-  return (
-    <>
+    if (!response.ok) {
+      throw new Error('Failed to fetch anime data');
+    }
+
+    const searchAnime = await response.json();
+
+    return (
       <section>
         <HeaderAnimeList title={`Pencarian Untuk ${keyword}.....`} />
         <AnimeList animes={searchAnime} />
       </section>
-    </>
-  )
+    );
+  } catch (error) {
+    return (
+      <section>
+        <HeaderAnimeList title="Error" />
+        <p>Gagal memuat data pencarian. Silakan coba lagi nanti.</p>
+        <p>{`Error : ${error}`}</p>
+      </section>
+    );
+  }
 }
 
 export default Page
