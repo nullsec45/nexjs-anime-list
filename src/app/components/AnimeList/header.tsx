9aa29f16c0f01734c05807c7812cdf84268f9439
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-15 15:31:24 +0700

    modified : add conditiional if not title and href heeader anime list.

--- src/app/components/AnimeList/header.tsx
+++ src/app/components/AnimeList/header.tsx
@@ -1,18 +1,22 @@
 import Link from "next/link"
 
 interface HeaderProps{
-    title:string;
-    linkHref:string;
-    linkTitle:string;
+    title?:string;
+    linkHref?:string;
+    linkTitle?:string;
 }
 
 const HeaderAnimeList:React.FC<HeaderProps>  = ({title,linkHref,linkTitle}) => {
   return (
     <div className="container mx-auto items-center flex justify-between items-center p-4">
-        <h1 className="text-2xl text-center">{title}</h1>
-        <Link href={linkHref} className="md:text-xl text-md underline hover:text-indigo-500 transition-all">
-            {linkTitle}
-        </Link>
+        <h1 className="text-2xl text-center text-color-primary">{title}</h1>
+        { linkHref && linkTitle ?
+            <Link href={linkHref} className="md:text-xl text-md underline hover:text-color-accent text-color-primary transition-all">
+              {linkTitle}
+            </Link>
+            :
+            null
+        }
     </div>
   )
 }
