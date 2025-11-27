c1c6c4947523224504cf21473e9d3d5f8b65aadc
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-11-27 22:14:29 +0700

    feat(utilities) : add header menu

---
+++ src/components/Utilities/HeaderMenu.tsx
@@ -0,0 +1,15 @@
+interface HeaderMenuProps {
+    title: string;
+}
+
+const HeaderMenu=({title}: HeaderMenuProps) => {
+    return (
+        <div>
+            <div className="p-8">
+                <h3 className="text-center text-2xl text-color-primary">{title}</h3>
+            </div>
+        </div>
+    )
+}
+
+export default HeaderMenu;
