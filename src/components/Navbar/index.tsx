d277d533a487751891bf9747d5a387ced2f190db
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-11-27 22:10:27 +0700

    refactor : move navbar to components folder.

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+// import InputSearch from "@/app/components/Utilities/Navbar/InpuSearch";
+// import InputSearch from "./InputSearch";
+import InputSearch from "@/components/Navbar/InputSearch";
+
+const Navbar=() => {
+    return (
+       <header className="bg-indigo-500">
+            <div className="flex md:flex-row flex-col justify-between md:items-center p-4 bg-color-accent">
+                <Link href='/' className="font-bold text-white text-2xl">AnimeList</Link>
+                <InputSearch />
+            </div>
+       </header>
+    )
+}
+
+export default Navbar;
